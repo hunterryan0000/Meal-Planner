b79c84b26fcb098e5a3791094410af5f7e244267
Marcello Hwang <marcellohhwang@gmail.com>
2022-04-13 14:11:51 -0500

    finalnav

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,7 +4,20 @@
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <router-link class="navbar-brand" v-bind:to="{ name: 'home' }">Home</router-link>
-          <router-link class="navbar-brand" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+          <div class="" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="nav-link" v-bind:to="{ name: 'recipes' }">Recipes</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" v-bind:to="{ name: 'create-recipe' }">Add Recipe</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+              </li>
+            </ul>
+
+          </div>
         </div>
       </nav>
     </div>
