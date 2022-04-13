c2e7d1fe2a8ab86b230bc860b447fc2d776901b5
Ryan Hunter <hunterryan0000@yahoo.com>
2022-04-13 10:50:50 -0500

    recipe search controller

--- java/src/main/java/com/techelevator/controller/RecipeSearchController.java
+++ java/src/main/java/com/techelevator/controller/RecipeSearchController.java
@@ -1,10 +1,27 @@
 package com.techelevator.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.dao.IngredientDao;
+import com.techelevator.dao.RecipeDao;
+import com.techelevator.model.Ingredient;
+import com.techelevator.model.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin
 @RestController
 public class RecipeSearchController {
 
+    @Autowired
+    private RecipeDao recipeDao;
+
+    public RecipeSearchController(RecipeDao recipeDao) {this.recipeDao = recipeDao;}
+
+    @RequestMapping(path = "/recipe/search/{term}", method = RequestMethod.GET)
+    public List<Recipe> search(@PathVariable String term){
+        return recipeDao.getRecipes(term);
+    }
 
 
 }
--- java/src/main/java/com/techelevator/dao/RecipeDao.java
+++ java/src/main/java/com/techelevator/dao/RecipeDao.java
@@ -1,5 +1,6 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Ingredient;
 import com.techelevator.model.Recipe;
 
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.List;
 public interface RecipeDao {
 
     List<Recipe> getAll(Long userId);
+
+    List<Recipe> getRecipes(String searchTerm);
 
     Recipe getRecipeById(Long recipeId, Long userId);
 
