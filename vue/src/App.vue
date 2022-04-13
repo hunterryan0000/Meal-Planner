a6ba0477ab63a61433a7c821ed4792caa09d60dc
Ryan Hunter <hunterryan0000@yahoo.com>
2022-04-13 12:13:07 -0500

    hamburger menu

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
+      <div class="container" v-on:click.prevent="myFunction(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </div>
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
@@ -8,3 +13,53 @@
   </div>
 </template>
 
+
+<script>
+
+export default {
+  methods: {
+    myFunction(x) {
+// Fix function to be hamburge... if/else ?
+      x.classList.add("change");
+    }
+  }
+}
+</script>
+
+
+<style>
+#nav {
+  background-color: beige;
+  display: flex;
+  
+}
+.container {
+  display: inline-block;
+  cursor: pointer;
+  background-color: black;
+  
+}
+
+.bar1, .bar2, .bar3 {
+ width: 35px;
+  height: 5px;
+  background-color: white;
+ margin: 6px 0;
+ transition: 0.4s;
+}
+/* Rotate first bar */
+.change .bar1 {
+ -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
+ transform: rotate(-45deg) translate(-9px, 6px) ;
+}
+/* Fade out the second bar */
+.change .bar2 {
+ opacity: 0;
+}
+/* Rotate last bar */
+.change .bar3 {
+  -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+  transform: rotate(45deg) translate(-8px, -8px) ;
+}
+
+</style>
