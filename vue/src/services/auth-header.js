c9029899694b73e9c58e23c3183201a9db868d1c
Matthew Broyles <Matthew.Broyles99@gmail.com>
2022-04-15 12:01:23 -0500

    Test

--- vue/src/services/auth-header.js
+++ vue/src/services/auth-header.js
@@ -4,8 +4,10 @@ export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
     let token = localStorage.getItem('token');
     if(user && token) {
+        console.log("WOOT")
         return { Authorization: 'Bearer ' + token};
     } else {
+        console.log("pensive")
         return {};
     }
 }
