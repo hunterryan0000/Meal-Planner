8b2f5fd326b26ac933a56ce2f2c52cda555d5c10
David Shafer <dhshafer.73@gmail.com>
2022-04-15 10:24:38 -0500

    merging conflict

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -29,11 +29,11 @@
 </template>
 
 <style scoped>
-#app {
+/* #app {
   background-image: url('https://cdn.discordapp.com/attachments/872641665314222100/963544684028895253/wood.jpg');
   height: 100vh;
   width: 100vw;
-}
+} */
 
 /* #nav {
   margin-bottom: 5%;
